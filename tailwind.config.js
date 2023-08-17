09a3004b0048de9c499fa70e191706c1580b7c9c
Juzer Shakir <juzershakir.webdev@gmail.com>
2023-08-17 20:01:37 +0530

    use hsla function

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,10 +12,10 @@ module.exports = {
     },
     extend: {
       colors: {
-        strongCyan: "hsl(171, 66%, 44%)",
-        lightBlue: "hsl(233, 100%, 69%)",
-        darkGrayishBlue: "hsl(210, 10%, 33%)",
-        grayishBlue: "hsl(201, 11%, 66%)",
+        strongCyan: "hsla(171, 66%, 44%, 1)",
+        lightBlue: "hsla(233, 100%, 69%, 1)",
+        darkGrayishBlue: "hsla(210, 10%, 33%, 1)",
+        grayishBlue: "hsla(201, 11%, 66%, 1)",
       },
       fontFamily: {
         sans: ["Bai Jamjuree", "sans-serif"],
